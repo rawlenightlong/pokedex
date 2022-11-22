ef32ccf4392b6b9e18481b310139706f8da94711
Rawle Becerril <rawlesxm@gmail.com>
2022-11-21 22:49:49 -0500

    finished pokedex app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT
 const pokemon = require('./models/pokemon')
 const methodOverride = require('method-override')
-const { type } = require('os')
+
 console.log(pokemon[0].id)
 
 
@@ -54,9 +54,27 @@ app.post('/pokemon', (req, res) => {
 })
 
 //Update Route
-app.put('/pokemon:id', (req, res) => {
-    res.send(`Pokemon Update Route`)
-})
+app.put('/pokemon/:id', (req, res) => {
+  
+    req.body.stats.hp = parseInt(req.body.stats.hp)
+    req.body.stats.attack = parseInt(req.body.stats.attack)
+    req.body.stats.defense = parseInt(req.body.stats.defense)
+    let type = req.body.type
+    let typeArray = (type.split(" "))
+    if (typeArray.length > 1){
+        typeArray[0] = (typeArray[0].substring(0, typeArray[0].length - 1))
+        req.body.type = typeArray
+    console.log(req.body)
+    pokemon[parseInt(req.params.id-1)].name = req.body.name
+    pokemon[parseInt(req.params.id-1)].type = req.body.type
+    pokemon[parseInt(req.params.id-1)].stats.hp = req.body.stats.hp
+    pokemon[parseInt(req.params.id-1)].stats.attack = req.body.stats.attack
+    pokemon[parseInt(req.params.id-1)].stats.defense = req.body.stats.defense
+
+    res.redirect('/pokemon/')
+}})
+
+
 //Edit Route
 app.get('/pokemon/:id/edit', (req, res) => {
     res.render('edit.ejs', {
