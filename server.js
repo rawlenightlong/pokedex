0223422d37e9da68c94556224826be51648628eb
Rawle Becerril <rawlesxm@gmail.com>
2022-12-03 00:10:22 -0500

    downloaded/required morgan

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT
 const pokemon = require('./models/pokemon')
+const morgan = require('morgan')
 const methodOverride = require('method-override')
 
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride("_method"))
 app.use('/static', express.static('public'))
+app.use(morgan('dev'))
 
 ////////////
 //Routes
